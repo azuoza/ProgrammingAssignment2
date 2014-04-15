e586df622e20748d74e6cd4caf0246ab3c64f1e5
AZuoza <azuoza@gmail.com>
2014-04-15 23:52:06 +0200

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Testing git
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {

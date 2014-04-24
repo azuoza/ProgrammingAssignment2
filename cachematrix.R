f62ec65d09a4cdf29d0a0640ba9d59c760a0448e
AZuoza <azuoza@gmail.com>
2014-04-24 19:35:59 +0200

    final upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,56 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Testing git
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache 
+## its inverse.
+makeCacheMatrix <- function(X = matrix()) {
+    inv <- NULL
+    # if matrix is not set, settig a matrix
+    set <- function(y){
+        X <<- y
+        inv <<- NULL
+    }
+    
+    # getting matrix
+    get <- function() X
+    
+    # setting and getting inverse of the matrix
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    
+    list(set=set, get=get,
+         setinv=setinv, 
+         getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## expected input: square invertible matrix
+## expected output: matrix that is the inverse of 'x'
+
+
+cacheSolve <- function(X, ...) {
+    
+    # checking if matrix is squared
+#    if (dim(X)[1] != dim(X[2])){
+#       print("matrix is not square!!")
+#        return
+#    }
+    
+    inv <- X$getinv()
+    
+    #checking if invers is done 
+    if(!is.null(inv)){
+        message("getting cached matrix")
+        return(inv)
+    }
+    data <- X$get()
+    inv <- solve(data, ...)
+    X$setinv(inv)
+    inv
+    
+    
 }
+cachemean(makeVector(1:10))
+cacheSolve(makeCacheMatrix(z))
